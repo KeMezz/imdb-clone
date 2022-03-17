4bc199bd9a4045affdee644fae3b4846dc60fd20
정형진 <pnescene@icloud.com>
2022-03-17 11:41:12 +0900

    styled-components 적용

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: dodgerblue;
+  font-size: 38px;
+  font-weight: bold;
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", { screen: "three" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "three" })}>
+    <Title>Movies</Title>
+  </Btn>
 );
 
 export default Movies;
